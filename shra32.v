10a89ae701f8c027a81fcaec191cf9eb8411ad0c
NiketKakkar <126024334+NiketKakkar03@users.noreply.github.com>
2025-02-10 20:39:43 -0500

    Update shra32.v
    shra operation bit change

--- shra32.v
+++ shra32.v
@@ -1,6 +1,6 @@
 module shra32 (
     input [31:0] Ra,       
-    input [31:0] shift_amt, 
+    input [4:0] shift_amt, 
     output [31:0] result   
 );
     assign result = $signed(Ra) >>> shift_amt; 
