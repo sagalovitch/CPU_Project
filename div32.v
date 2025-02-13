dde40363dc6eebb912044442a5bba17c1acf0708
HetBuddhdev10 <hetbuddhdev10@gmail.com>
2025-02-13 11:45:36 -0500

    Update div32.v for signed division

--- div32.v
+++ div32.v
@@ -1,37 +1,60 @@
 `timescale 1ns/10ps
 
 module div32 (
-    input wire [31:0] A,   // Dividend Q
-    input wire [31:0] M,   // Divisor M
-    output reg [31:0] Q,   // Quotient Q/M
-    output reg [31:0] R    // Remainder 
+    input wire signed [31:0] A,   // Dividend 
+    input wire signed [31:0] M,   // Divisor 
+    output reg signed [31:0] Q,   // Quotient 
+    output reg signed [31:0] R    // Remainder
 );
 
-    reg [63:0] temp;  
-    integer i;  
+    reg [63:0] temp; 
+    reg [31:0] absA, absM;  
+    reg signQ, signR;  
+    integer i; 
 
     always @(*) begin
-        Q = 0;
-        temp = {32'b0, A};  
-
+     
         if (M == 0) begin
             Q = 32'hFFFFFFFF;  
             R = 32'hFFFFFFFF;
-        end else begin
+        end 
+        else if (A == 32'h80000000 && M == -1) begin
+            Q = 32'h80000000;  
+            R = 0;  
+        end 
+        else begin
+            
+            signQ = A[31] ^ M[31]; 
+            signR = A[31];         
+
+            absA = (A[31]) ? (~A + 1) : A; 
+            absM = (M[31]) ? (~M + 1) : M; 
+
+            Q = 0;
+            temp = {32'b0, absA}; 
+
             for (i = 0; i < 32; i = i + 1) begin
                 temp = temp << 1;
 
-                temp[63:32] = temp[63:32] - M;
+      
+                temp[63:32] = temp[63:32] - absM;
 
                 if (temp[63] == 1) begin
-
-                    temp[63:32] = temp[63:32] + M;
+                    temp[63:32] = temp[63:32] + absM;
                     Q = Q << 1;  
                 end else begin
-                    Q = (Q << 1) | 1; 
+                    Q = (Q << 1) | 1;  
                 end
             end
+       
             R = temp[63:32];
+
+       
+            if (signQ)
+                Q = ~Q + 1;  
+
+            if (signR)
+                R = ~R + 1;  
         end
     end
 
