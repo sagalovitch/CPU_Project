9f6acc841f67b0aba7abf347a69d82f328627585
HetBuddhdev10 <hetbuddhdev10@gmail.com>
2025-02-26 14:43:10 -0500

    Update sub32.v

--- sub32.v
+++ sub32.v
@@ -1,21 +1,27 @@
-`timescale 1ns/10ps 
+`timescale 1ns/10ps  
 
 module sub32 (
-    input wire [31:0] A, B,  
-    input wire Cin,          
-    output wire [31:0] Diff, 
-    output wire Cout         
+    input wire [31:0] A, B, 
+    output wire [31:0] Diff,
+    output wire Cout,   
+    output wire Overflow   
 );
 
     wire [31:0] B_complement; 
-    assign B_complement = ~B + 1; 
+    wire Cout_internal; 
+    
+    assign B_complement = ~B;
 
     add32 cla_adder (
         .A(A),
         .B(B_complement),
-        .Cin(1'b1), 
+        .Cin(1'b1),  
         .Sum(Diff),
-        .Cout(Cout)  
+        .Cout(Cout_internal)  
     );
 
+    assign Cout = ~Cout_internal; 
+    assign Overflow = (A[31] ^ B[31]) & (A[31] ^ Diff[31]); 
+
 endmodule
+
