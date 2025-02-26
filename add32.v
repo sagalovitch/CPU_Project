75ca6fe1ad12d323ec25afb44fd417d0d248f46e
HetBuddhdev10 <hetbuddhdev10@gmail.com>
2025-02-26 14:17:50 -0500

    Update add32.v

--- add32.v
+++ add32.v
@@ -1,14 +1,14 @@
-`timescale 1ns/10ps  
-
+ `timescale 1ns/10ps  
 
 module cla_4bit_adder (
-    input wire [3:0] A, B,  
+    input wire [3:0] A, B,
     input wire Cin,         
-    output wire [3:0] Sum,  
-    output wire Cout        
+    output wire [3:0] Sum,
+    output wire Cout      
 );
-    wire [3:0] G, P, C;
+    wire [3:0] G, P, C; 
 
+ 
     assign G = A & B;
     assign P = A ^ B;
 
@@ -26,13 +26,12 @@ endmodule
 
 module cla_16bit_adder (
     input wire [15:0] A, B,  
-    input wire Cin,          
-    output wire [15:0] Sum,  
-    output wire Cout         
+    input wire Cin,       
+    output wire [15:0] Sum, 
+    output wire Cout    
 );
-    wire C4, C8, C12;  
+    wire C4, C8, C12; 
 
-  
     cla_4bit_adder adder0(A[3:0], B[3:0], Cin, Sum[3:0], C4);
     cla_4bit_adder adder1(A[7:4], B[7:4], C4, Sum[7:4], C8);
     cla_4bit_adder adder2(A[11:8], B[11:8], C8, Sum[11:8], C12);
@@ -40,16 +39,18 @@ module cla_16bit_adder (
 
 endmodule
 
-
 module add32 (
-    input wire [31:0] A, B, 
+    input wire [31:0] A, B,
     input wire Cin,         
-    output wire [31:0] Sum, 
-    output wire Cout        
+    output wire [31:0] Sum,  
+    output wire Cout,     
+    output wire Overflow   
 );
-    wire C16;  
+    wire C16; 
 
     cla_16bit_adder adder_low(A[15:0], B[15:0], Cin, Sum[15:0], C16);
     cla_16bit_adder adder_high(A[31:16], B[31:16], C16, Sum[31:16], Cout);
+
+    assign Overflow = (A[31] & B[31] & ~Sum[31]) | (~A[31] & ~B[31] & Sum[31]);
 
 endmodule
